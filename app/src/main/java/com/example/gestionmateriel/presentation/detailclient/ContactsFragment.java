aa69920ffc21689812686af790725a706cfcfa64
maxime <17.laurinmaxime@gmail.com>
2019-07-08 15:55:38 +0200

    Fragments

--- app/src/main/java/com/example/gestionmateriel/presentation/detailclient/ContactsFragment.java
+++ app/src/main/java/com/example/gestionmateriel/presentation/detailclient/ContactsFragment.java
@@ -1,6 +1,21 @@
 package com.example.gestionmateriel.presentation.detailclient;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.gestionmateriel.R;
+
 public class ContactsFragment extends Fragment {
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.contacts_layout, container, false);
+        return root;
+    }
 }
--- app/src/main/java/com/example/gestionmateriel/presentation/detailclient/MaterielsClientsFragment.java
+++ app/src/main/java/com/example/gestionmateriel/presentation/detailclient/MaterielsClientsFragment.java
@@ -1,6 +1,21 @@
 package com.example.gestionmateriel.presentation.detailclient;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.gestionmateriel.R;
+
 public class MaterielsClientsFragment extends Fragment {
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.materiels_clients_layout, container, false);
+        return root;
+    }
 }
