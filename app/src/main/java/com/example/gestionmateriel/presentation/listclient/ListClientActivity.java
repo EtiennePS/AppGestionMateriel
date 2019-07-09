a336febcebaca91556c38461fc0fe375a005891b
EtiennePS <etienne.picherit@gmail.com>
2019-07-09 15:15:42 +0200

    popup pour test http

--- app/src/main/java/com/example/gestionmateriel/presentation/listclient/ListClientActivity.java
+++ app/src/main/java/com/example/gestionmateriel/presentation/listclient/ListClientActivity.java
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.gestionmateriel.R;
 import com.example.gestionmateriel.entite.Client;
 import com.example.gestionmateriel.presentation.detailclient.ListeClientsAdapter;
@@ -18,36 +21,6 @@ import java.util.List;
 
 public class ListClientActivity extends AppCompatActivity {
 
-   /* @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_client);
-
-        update();
-    }
-
-    private void update() {
-
-        Response.Listener<String> rl = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                TextView tv = (TextView) findViewById(R.id.testRequete);
-                tv.setText(response);
-            }
-        };
-
-        Response.ErrorListener rel = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                TextView tv = (TextView) findViewById(R.id.testRequete);
-                tv.setText(error.getMessage());
-            }
-        };
-
-        RequestMaker rm = new RequestMaker();
-        rm.requestCombinedJSON(getApplicationContext(), rl, rel);
-    }*/
-
     private RecyclerView recyclerView;
 
     private List<Client> clients = new ArrayList<>();
@@ -57,14 +30,14 @@ public class ListClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_client);
         ajouterClient();
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
 
         recyclerView.setAdapter(new ListeClientsAdapter(clients) {
         });
         Log.i("test","test"+  clients.toString() );
+
+        update();
     }
 
     private void ajouterClient() {
@@ -79,5 +52,26 @@ public class ListClientActivity extends AppCompatActivity {
         Intent mainIntent = new Intent(this, ListClientActivity.class);
         startActivity(mainIntent);
         finish();
+    }
+
+    private void update() {
+
+        Response.Listener<String> rl = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("TEST", response);
+                Toast.makeText(ListClientActivity.this, response, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Response.ErrorListener rel = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ListClientActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        RequestMaker rm = new RequestMaker();
+        rm.requestCombinedJSON(getApplicationContext(), rl, rel);
     }
 }
