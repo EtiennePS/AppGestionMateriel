f10cff80e2f160f82ff560bbbc2cf6439598498c
maxime <17.laurinmaxime@gmail.com>
2019-07-10 15:51:26 +0200

    Ajout Repository Materiel

---
+++ app/src/main/java/com/example/gestionmateriel/repository/MaterielRepository.java
@@ -0,0 +1,35 @@
+package com.example.gestionmateriel.repository;
+
+import com.example.gestionmateriel.entite.Client;
+import com.example.gestionmateriel.entite.Materiel;
+
+import java.util.List;
+
+public class MaterielRepository {
+
+    private static MaterielRepository instance;
+    private MaterielRepository() { }
+
+    public static MaterielRepository getInstance() {
+        if(instance == null) {
+            instance = new MaterielRepository();
+        }
+        return instance;
+    }
+
+    public Materiel getById(int id) {
+        ClientRepository cr = ClientRepository.getInstance();
+
+        for(  Client c:cr.getAll()) {
+            for(Materiel m : c.getMateriels()) {
+                if (m.getId() == id){
+                    return m;
+                }
+
+            }
+        }
+
+        return null;
+    }
+
+}
