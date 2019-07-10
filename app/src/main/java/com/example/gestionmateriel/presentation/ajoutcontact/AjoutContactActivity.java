94ab398aae62348287699c7cdfae5b2a69afcac4
maxime <17.laurinmaxime@gmail.com>
2019-07-10 14:38:08 +0200

    Ajout du Formulaire d'ajout de contact

---
+++ app/src/main/java/com/example/gestionmateriel/presentation/ajoutcontact/AjoutContactActivity.java
@@ -0,0 +1,15 @@
+package com.example.gestionmateriel.presentation.ajoutcontact;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.gestionmateriel.R;
+
+import android.os.Bundle;
+
+public class AjoutContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_contact);
+    }
+}
